579eccd7817ceead348b30aa14b8566701f1ffbe
Estifanose Sahilu <estifanoswork@gmail.com>
2025-04-10 22:27:30 +0300

    perf: :memo: Move personal info from profile to user for better performance

--- src/models/profile.model.ts
+++ src/models/profile.model.ts
@@ -9,15 +9,6 @@ const Profile = new mongoose.Schema({
     attendance: [{ type: mongoose.Schema.Types.ObjectId, ref: "Attendance" }],
     progress: [{ type: mongoose.Schema.Types.ObjectId, ref: "Progress" }],
     heads_ups: [{ type: mongoose.Schema.Types.ObjectId, ref: "HeadsUp" }],
-    personal_info: {
-        bio: { type: String },
-        instagram_handle: { type: String },
-        linkedin_handle: { type: String },
-        leetcode_handle: { type: String },
-        codeforce_handle: { type: String },
-        cv_link: { type: String },
-        resources: [{ resource_name: String, resource_link: String }],
-    },
 });
 
 export default mongoose.model("Profile", Profile);
