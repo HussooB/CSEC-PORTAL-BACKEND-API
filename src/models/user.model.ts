d40994d44b5a4b763212402f1e5bebef521c716c
Estifanose Sahilu <estifanoswork@gmail.com>
2025-04-10 18:22:21 +0300

    feat: Add User model

---
+++ src/models/user.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const User = new mongoose.Schema(
+    {
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        current_profile_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Profile",
+            default: null,
+        },
+        personal_info: {
+            first_name: { type: String },
+            last_name: { type: String },
+            gender: { type: String },
+            birth_date: { type: Date },
+            phone_number: { type: String },
+            github_handle: { type: String },
+            telegram_handle: { type: String },
+            department: { type: String },
+            specialization: { type: String },
+            graduation_year: { type: Number },
+            university_id: { type: String },
+        },
+        profile_list: [
+            { type: mongoose.Schema.Types.ObjectId, ref: "Profile" },
+        ],
+    },
+    { timestamps: true }
+);
+
+const UserModel = mongoose.model("User", User);
+export default UserModel;
