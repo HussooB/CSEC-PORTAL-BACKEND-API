3f29a8f38bba58a7738d918ed9e5a37eb989aeed
Estifanose Sahilu <estifanoswork@gmail.com>
2025-04-10 18:31:41 +0300

    feat: Add controller blueprints for user

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,58 @@
+import UserModel from "../models/user.model";
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        res.send("Creating user");
+    } catch (error) {
+        res.status(500).json({
+            error: "An error occurred while creating the user.",
+            details: error,
+        });
+    }
+};
+
+export const getUser = async (req: Request, res: Response) => {
+    try {
+        res.send("Fetching user");
+    } catch (error) {
+        res.status(500).json({
+            error: "An error occurred while fetching the user.",
+            details: error,
+        });
+    }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+    try {
+        res.send("Updating user");
+    } catch (error) {
+        res.status(500).json({
+            error: "An error occurred while updating the user.",
+            details: error,
+        });
+    }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+        res.send("Deleting user");
+    } catch (error) {
+        res.status(500).json({
+            error: "An error occurred while deleting the user.",
+            details: error,
+        });
+    }
+};
+
+export const listUsers = async (req: Request, res: Response) => {
+    try {
+        res.send("Listing users");
+    } catch (error) {
+        res.status(500).json({
+            error: "An error occurred while listing the users.",
+            details: error,
+        });
+    }
+};
